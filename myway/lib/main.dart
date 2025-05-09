da416a058f29b572a03acc6145f109dddb1d57f0
bhyn <bhyn9785@gmail.com>
2025-05-09 11:28:20 +0900

    브랜치명 변경 테스트 커밋

--- myway/lib/main.dart
+++ myway/lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(fontFamily: 'Freesentation'),
       home: const HomeScreen(),
+      //test commit
     );
   }
 }
