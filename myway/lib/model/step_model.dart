991d8e7e0be3b78493cd3948ed7f4f81eb0682a1
DeokWon Kim <khjs7878@naver.com>
2025-05-19 13:31:11 +0900

    feat: StepModel에 종료시간 필드 추가 및 JSON 변환 메서드 구현

--- myway/lib/model/step_model.dart
+++ myway/lib/model/step_model.dart
@@ -2,10 +2,25 @@ class StepModel {
   final int steps;
   final String duration;
   final String distance;
+  final String stopTime;
 
   StepModel({
     required this.steps,
     required this.duration,
     required this.distance,
+    required this.stopTime,
   });
+
+  Map<String, dynamic> toJson() {
+    return {'걸음수': steps, '소요시간': duration, '거리': distance, '종료시간': stopTime};
+  }
+
+  factory StepModel.fromJson(Map<String, dynamic> json) {
+    return StepModel(
+      steps: json['걸음수'],
+      duration: json['소요시간'],
+      distance: json['거리'],
+      stopTime: json['종료시간'],
+    );
+  }
 }
